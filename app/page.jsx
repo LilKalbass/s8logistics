4e2ff8164bd3ba7f2db3a0d6d9f4c983377a3517
Artem Dimoglov <adimoglov@gmail.com>
2024-04-08 11:45:36 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+      <main className = 'w-full max-w-[1440px] bg-white mx-auto overflow-hidden'>
+          <Header/>
+      </main>
+  );
+}
+
+// Yu Gothic UI
+// RGB  0, 145, 106 CMYK  83, 17, 69, 3
+// RGB  135, 135, 135 CMYK  47, 37, 37, 17
+// RGB  60, 60, 60 CMYK  67, 57, 54, 60
